40ad5db5ab883bce182d79533bc56997fc34db13
Yeray <yeray14sj@gmail.com>
2021-04-13 10:44:51 +0200

    Segunda version

--- Repositorio/src/alumno/alumno.java
+++ Repositorio/src/alumno/alumno.java
@@ -13,6 +13,22 @@ public class alumno {
     
     private String nombre;
     private String apellidos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
     
     
 }
