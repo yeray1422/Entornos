95ff70fea31353da60553f6ba1d93734bda8d188
Yeray <yeray14sj@gmail.com>
2021-04-13 10:52:27 +0200

    Cambio de nombre del archivo

---
+++ Repositorio/src/alumno/Alumno.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alumno;
+
+/**
+ *
+ * @author sanaguye
+ */
+public class Alumno {
+    
+    private String nombre;
+    private String apellidos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
+}
